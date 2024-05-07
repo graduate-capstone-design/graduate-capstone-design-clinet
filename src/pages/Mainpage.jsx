fe0626095fa0e27069007439761df41f0109cf52
gunyongbok <qhrrjsdyd123@gmail.com>
2024-05-07 16:22:33 +0900

    feat: 메인 페이지 검색 창 구현

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
-import ResponsiveAppBar from "../components/Nav/ResponsiveAppBar";
+
+// style
 import styles from "./style/Mainpage.module.css";
 
+// Components
+import ResponsiveAppBar from "../components/Nav/ResponsiveAppBar";
+
+// Svg
+import searchIcon from "../asset/svg/search.svg";
+
+const data = { 음식점: "restaurant", 레시피: "recipe" };
+
 const MainPage = () => {
-  const data = { 음식점: "restaurant", 레시피: "recipe" };
   const [search, setSearch] = useState("");
+  const [inputValue, setInputValue] = useState("");
 
   // 음식점 버튼 클릭 핸들러
   const handleRestaurantClick = () => {
@@ -16,7 +25,11 @@ const MainPage = () => {
     setSearch(data["레시피"]);
   };
 
-  console.log(search);
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  console.log(inputValue);
 
   return (
     <div className={styles.top}>
@@ -30,10 +43,22 @@ const MainPage = () => {
             <button className={styles.btn} onClick={handleRestaurantClick}>
               음식점
             </button>
-
             <button className={styles.btn} onClick={handleRecipeClick}>
               레시피
             </button>
+          </div>
+        </div>
+        <div className={styles.inputContainer}>
+          <input
+            className={styles.input}
+            required
+            type="text"
+            placeholder="원하는 음식을 검색해주세요."
+            value={inputValue}
+            onChange={handleChange}
+          />
+          <div className={styles.iconContainer}>
+            <img src={searchIcon} alt="Search" className={styles.icon} />
           </div>
         </div>
       </div>
